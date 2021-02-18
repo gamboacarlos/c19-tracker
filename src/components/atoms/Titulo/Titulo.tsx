9b25d51460c20fe2071a1b9c2515e93555390bd7
Carlos Gamboa <carlosgamboa@mail.com>
2021-02-18 15:14:13 +0100

    webpack configuration ready

--- src/components/atoms/Titulo/Titulo.tsx
+++ src/components/atoms/Titulo/Titulo.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import styles from './Titulo.module.scss';
 
 type Props = {
-    children: string
-}
+  children: ReactNode;
+};
 
 const Titulo: FC<Props> = ({ children }) => (
   <div className={styles.title}>
@@ -12,36 +12,3 @@ const Titulo: FC<Props> = ({ children }) => (
 );
 
 export default Titulo;
-
-// module.exports = {
-//     env: {
-//       browser: true,
-//       es2021: true,
-//       node: true
-//     },
-//     extends: [
-//       'plugin:@typescript-eslint/recommended',
-//       'plugin:react/recommended'
-//     ],
-//     parser: '@typescript-eslint/parser',
-//     parserOptions: {
-//       ecmaFeatures: {
-//         jsx: true
-//       },
-//       ecmaVersion: 12,
-//       sourceType: 'module'
-//     },
-//     plugins: [
-//       'react',
-//       '@typescript-eslint'
-//     ],
-//     rules: {
-//       'no-unused-vars': 0,
-//       'react/prop-types': ['off'],
-//     },
-//     settings: {
-//       "react": {
-//         "version": "detect"
-//       }
-//     }
-//   }
